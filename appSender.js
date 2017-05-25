6192b7b749682df27f2d61fd1c360107c95bb13b
nowido <nowido@mail.ru>
2017-05-25 21:15:31 +0300

    Update appSender.js

--- appSender.js
+++ appSender.js
@@ -1,21 +1,20 @@
 //--------------------------------------------------------------------------------------------
 
 $(() => 
-{
-    var fb = new FluidBridge();
-
-    $('body').append('<p><label>My key: <input id="myKey" type="text"></label><p><label>Alien key: <input id="alienKey" type="text"></label><p><button id="buttonSend">Send</button>');
-
+{    
     $('#buttonSend').click(() => 
     {
-        var keyPair = $('#myKey').val() + $('#alienKey').val();
+        var from = $('#myKey').val();
+        var to = $('#alienKey').val();
 
-        fb.listenBridgeOff(keyPair, () => 
+        var fb = new FluidBridge(from, to);
+
+        fb.listenPacketRemoved(() => 
         {
-            console.log('Transferred OK');
+            alert('Transferred OK');
         }); 
 
-        fb.post(keyPair, 'data packet');        
+        fb.post($('#messageText').val());        
     });
 });
 
