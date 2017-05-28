c23a0c4bc91da2038334e1cb4208493c84a9fb7a
nowido <nowido@mail.ru>
2017-05-28 13:14:21 +0300

    Add files via upload

---
+++ sink.js
@@ -0,0 +1,38 @@
+//--------------------------------------------------------------------------------------------
+
+$(() => 
+{
+    var sinkFbKeyElement = $('#sinkFbKey');
+
+    var buttonListenElement = $('#buttonListen');
+
+    var messagesElement = $('#messages');
+
+    var sinkKey;
+    
+    var fsink;
+
+    buttonListenElement.click(() => 
+    {
+        sinkKey = sinkFbKeyElement.val();
+        
+        fsink = new FluidSink(sinkKey);
+
+        fsink.onPacketOn((content) => 
+        {                        
+            messagesElement.val(messagesElement.val() + ': ' + content + '\n');            
+
+            messagesElement.scrollTop(messagesElement[0].scrollHeight);            
+        });
+        
+        fsink.listenPackets();
+        
+        sinkFbKeyElement.prop('disabled', true);
+        
+        buttonListenElement.prop('disabled', true);       
+
+        document.title = 'Fluid Sink (' + sinkKey + ')';  
+    });
+});
+
+//--------------------------------------------------------------------------------------------
