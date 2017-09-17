03ee277638bbbd260db4b6be584e7a9e4bcac88f
nowido <nowido@mail.ru>
2017-09-17 21:09:13 +0300

    Update gettask-eh.js

--- FirebasePubnub/gettask-eh.js
+++ FirebasePubnub/gettask-eh.js
@@ -4,48 +4,63 @@ export default (request) =>
     
     const comm = require('pubnub'); 
     const xhr = require("xhr"); 
-    
+        
+    function prepareOptTask(descriptor, destinationChannel)
+    {
+        const dbPrefix = 'https://fluidfunctions.firebaseio.com/';
+        
+        let resultKey = 'results/' + descriptor.codeKey;
+        
+        xhr.fetch(dbPrefix + resultKey + '.json')
+        .then((response) => {
+
+            let currentOpt = JSON.parse(response.body);
+
+            const bounds = {left: [-10, -10, -10], right: [10, 10, 10]};                
+            const N = 1000;
+                
+            const msg = 
+            {
+                entryPoint: descriptor.entryPoint,
+                codeKey: descriptor.codeKey,
+                data: {bounds: bounds, steps: [N, N, N], currentOpt: currentOpt}, 
+                result: resultKey
+            };     
+            
+            comm.publish({message: msg, channel: destinationChannel});                        
+        })
+        .catch(console.log);        
+    }
+
+    function prepareMonteCarloTask(descriptor, destinationChannel)
+    {
+        const N = 1000000000; // 1 billion of random points to calc
+        
+        const msg = 
+        {
+            entryPoint: descriptor.entryPoint,
+            codeKey: descriptor.codeKey,
+            data: {pointsCount: N}, 
+            result: 'pi'
+        };     
+
+        comm.publish({message: msg, channel: destinationChannel});                        
+    }
+
     const handlers = 
     [
-        {entryPoint: 'test1', codeKey: 'test1'},
-        {entryPoint: 'test2', codeKey: 'test2'}
+        {entryPoint: 'test1', codeKey: 'test1', taskBuilder: prepareOptTask},
+        {entryPoint: 'test2', codeKey: 'test2', taskBuilder: prepareOptTask},
+        {entryPoint: 'test3', codeKey: 'test3', taskBuilder: prepareMonteCarloTask}
     ];
 
     const feedbackChannel = request.message.feedbackChannel;
 
     if(feedbackChannel && (feedbackChannel.length > 0))
     {
-        let randomIndex = Math.floor(Math.random() * handlers.length);
+        let index = Math.floor(Math.random() * handlers.length);
         
-        let handler = handlers[randomIndex];
-    
-        let resultKey = 'opt3results' + randomIndex;
-
-        xhr.fetch('https://fluidfunctions.firebaseio.com/' + resultKey + '.json')
-            .then((response) => {
-
-                let currentOptX = JSON.parse(response.body);
-
-                const bounds = 
-                [
-                    {left: -10, right: 10}, 
-                    {left: -10, right: 10}, 
-                    {left: -10, right: 10}
-                ];
-                
-                const N = 1000;
-                    
-                const msg = 
-                {
-                    entryPoint: handler.entryPoint,
-                    codeKey: handler.codeKey,
-                    data: {bounds: bounds, steps: [N, N, N], currentOptX: currentOptX}, 
-                    result: resultKey
-                };     
-                
-                comm.publish({message: msg, channel: feedbackChannel});                        
-            })
-            .catch(console.log);
+        handlers[index].taskBuilder(handlers[index], feedbackChannel);
     }
             
     return request.ok(); 
